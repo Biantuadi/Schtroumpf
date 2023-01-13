aaa8f6b886d10176d3fc94181b5adac39ff74317
Kevin-beni <benibiantuadi@gmail.com>
2023-01-13 05:31:06 +0100

    🧐 B01: mongoDB.js > adiing db in my project

--- api/app.js
+++ api/app.js
@@ -4,7 +4,10 @@ const cors = require("cors");
 
 // middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json());7
+
+// DB
+require('./db/mongoDB');
 
 // routes
 const schtroumpfRoutes = require('./routes/schtroumpf.router');
