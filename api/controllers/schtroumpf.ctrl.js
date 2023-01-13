899d62233a2c51f67239007f799ae857b84c6348
Kevin-beni <benibiantuadi@gmail.com>
2023-01-13 22:51:48 +0100

    ✅ B02: creating comple crud

--- api/controllers/schtroumpf.ctrl.js
+++ api/controllers/schtroumpf.ctrl.js
@@ -2,7 +2,7 @@ const Schtroumpf = require("../models/schtroumpf.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-exports.loginSchtroumpf = (req, res, next) => {
+exports.loginSchtroumpf = (req, res) => {
   Schtroumpf.findOne({ email: req.body.email })
     .then((schtroumpf) => {
       if (!schtroumpf)
@@ -21,6 +21,7 @@ exports.loginSchtroumpf = (req, res, next) => {
             token: jwt.sign(
               {
                 userId: schtroumpf._id,
+                role: schtroumpf.role,
               },
               process.env.JWT_KEY,
               { expiresIn: "24h" }
@@ -32,7 +33,7 @@ exports.loginSchtroumpf = (req, res, next) => {
     .catch((err) => res.status(500).json({ error: err }));
 };
 
-exports.createSchtroumpf = (req, res, next) => {
+exports.createSchtroumpf = (req, res) => {
   bcrypt.hash(req.body.password, 10).then((hash) => {
     const newSchtroumpf = new Schtroumpf({
       name: req.body.name,
@@ -49,10 +50,55 @@ exports.createSchtroumpf = (req, res, next) => {
   });
 };
 
-exports.getAllSchtroumpfs = (req, res, next) => {};
+exports.getAllSchtroumpfs = (req, res) => {
+  Schtroumpf.find()
+    .select("-password")
+    .then((users) => res.status(200).json(users))
+    .catch((err) => res.status(500).json({ error: err }));
+};
 
-exports.getSchtroumpf = (req, res, next) => {};
+exports.getSchtroumpf = (req, res) => {
+  Schtroumpf.findOne({ _id: req.params.id })
+    .select("-password")
+    .then((user) => res.status(200).json(user))
+    .catch((err) => res.status(500).json({ error: err }));
+};
 
-exports.modifySchtroumpf = (req, res, next) => {};
+exports.modifySchtroumpf = (req, res) => {
+  // get user data
+  const token = req.headers.authorization.split(" ")[1];
+  const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+  const userId = decodedToken.userId;
 
-exports.deleteSchtroumpf = (req, res, next) => {};
+  Schtroumpf.findOne({ _id: userId }).then((schtroumpf) => {
+    const oldName = schtroumpf.name;
+    const oldRole = schtroumpf.role;
+
+    if (oldName === req.body.name || oldRole === req.body.role)
+      return res.status(400).json({ error: "Aucune modification !" });
+
+    Schtroumpf.updateOne(
+      { _id: req.params.id },
+      { ...req.body, _id: req.params.id }
+    )
+      .then(() => res.status(200).json({ message: "Utilisateur modifié !" }))
+      .catch((err) => res.status(500).json({ error: err }));
+  });
+};
+
+exports.deleteSchtroumpf = (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+  const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+  const userId = decodedToken.userId;
+  const role = decodedToken.role;
+
+  // if the user is an admin, delete the user
+  if (role === "admin" || userId === req.params.id) {
+    userModel
+      .deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: "Utilisateur supprimé !" }))
+      .catch((err) => res.status(500).json({ error: err }));
+  } else {
+    res.status(401).json({ error: "Vous n'êtes pas autorisé !" });
+  }
+};
